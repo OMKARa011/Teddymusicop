3cf15edd34b9146e1d38d6273b4d7263f9e739cb
SRUJANSBOT <97078395+SRUJANSBOT@users.noreply.github.com>
2022-07-21 08:35:37 +0530

    Update checker.py

--- Hero/Decorators/checker.py
+++ Hero/Decorators/checker.py
@@ -6,22 +6,22 @@ def checker(mystic):
     async def wrapper(_, message):
         if message.sender_chat:
             return await message.reply_text(
-                "ʏᴏᴜ'ʀᴇ ᴀɴ __ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ__ ɪɴ ᴛʜɪs ᴄʜᴀᴛ ɢʀᴏᴜᴘ...\nʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ғʀᴏᴍ ᴀᴅᴍɪɴ ʀɪɢʜᴛs..."
+                "Yᴏᴜ'ʀᴇ ᴀɴ __ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ__ ɪɴ ᴛʜɪs ᴄʜᴀᴛ/ɢʀᴏᴜᴘ...\nRᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ғʀᴏᴍ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ɪɴ ᴏʀᴅᴇʀ ᴛᴏ ᴜsᴇ ᴍᴇ..."
             )
         blacklisted_chats_list = await blacklisted_chats()
         if message.chat.id in blacklisted_chats_list:
             await message.reply_text(
-                f"**ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛ**\n\nʏᴏᴜʀ ᴄʜᴀᴛ ʜᴀs ʙᴇᴇɴ ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ʙʏ sᴜᴅᴏ ᴜsᴇʀs ᴀsᴋ ᴀɴʏ __SUDO USER__ ᴛᴏ ᴡʜɪᴛᴇʟɪsᴛ...\nᴄʜᴇᴄᴋ sᴜᴅᴏ ᴜsᴇʀs ʟɪsᴛ [ғʀᴏᴍ ʜᴇʀᴇ](https://t.me/{BOT_USERNAME}?start=sudolist)"
+                f"**Bʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛ**\n\nYᴏᴜʀ ᴄʜᴀᴛ ʜᴀs ʙᴇᴇɴ ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ʙʏ sᴜᴅᴏ ᴜsᴇʀs ᴀsᴋ ᴀɴʏ __SUDO USER__ ᴛᴏ ᴡʜɪᴛᴇʟɪsᴛ ʏᴏᴜʀ ᴄʜᴀᴛ...\nCʜᴇᴄᴋ sᴜᴅᴏ ᴜsᴇʀs ʟɪsᴛ [ғʀᴏᴍ ʜᴇʀᴇ](https://t.me/{BOT_USERNAME}?start=sudolist)"
             )
             return await app.leave_chat(message.chat.id)
         if await is_on_off(1):
             if int(message.chat.id) != int(LOG_GROUP_ID):
                 return await message.reply_text(
-                    f"ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ sᴏʀʀʏ ғᴏʀ ᴛʜᴇ ɪɴᴄᴏɴᴠᴇɴɪᴇɴᴄᴇ"
+                    f"Bᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ sᴏʀʀʏ ғᴏʀ ᴛʜᴇ ɪɴᴄᴏɴᴠᴇɴɪᴇɴᴄᴇ...Cᴏɴᴛᴀᴄᴛ ʙᴏᴛ ᴀᴅᴍɪɴs ❇️:- @Teddysupport"
                 )
         if await is_gbanned_user(message.from_user.id):
             return await message.reply_text(
-                f"**ɢʙᴀɴɴᴇᴅ ᴜsᴇʀ**\n\nʏᴏᴜ'ʀᴇ ɢʙᴀɴɴᴇᴅ ғʀᴏᴍ ᴜsɪɴɢ ʙᴏᴛ ᴀsᴋ ᴀɴʏ __SUDO USER__ ᴛᴏ ᴜɴɢʙᴀɴ...\nᴄʜᴇᴄᴋ sᴜᴅᴏ ᴜsᴇʀs ʟɪsᴛ [ғʀᴏᴍ ʜᴇʀᴇ](https://t.me/{BOT_USERNAME}?start=sudolist)"
+                f"**Gʙᴀɴɴᴇᴅ ᴜsᴇʀ**\n\nYᴏᴜ'ʀᴇ ɢʙᴀɴɴᴇᴅ ғʀᴏᴍ ᴜsɪɴɢ ʙᴏᴛ, ᴀsᴋ ᴀɴʏ __SUDO USER__ ᴛᴏ ᴜɴɢʙᴀɴ ʏᴏᴜ ғɪʀsᴛ...\nCʜᴇᴄᴋ sᴜᴅᴏ ᴜsᴇʀs ʟɪsᴛ [ғʀᴏᴍ ʜᴇʀᴇ](https://t.me/{BOT_USERNAME}?start=sudolist)"
             )
         return await mystic(_, message)
 
@@ -33,17 +33,17 @@ def checkerCB(mystic):
         blacklisted_chats_list = await blacklisted_chats()
         if CallbackQuery.message.chat.id in blacklisted_chats_list:
             return await CallbackQuery.answer(
-                "ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛ", show_alert=True
+                "Bʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛ", show_alert=True
             )
         if await is_on_off(1):
             if int(CallbackQuery.message.chat.id) != int(LOG_GROUP_ID):
                 return await CallbackQuery.answer(
-                    "ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ sᴏʀʀʏ ғᴏʀ ᴛʜᴇ ɪɴᴄᴏɴᴠᴇɴɪᴇɴᴄᴇ...",
+                    "Bᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ sᴏʀʀʏ ғᴏʀ ᴛʜᴇ ɪɴᴄᴏɴᴠᴇɴɪᴇɴᴄᴇ...Cᴏɴᴛᴀᴄᴛ ʙᴏᴛ ᴀᴅᴍɪɴs ❇️:- @Teddysupport",
                     show_alert=True,
                 )
         if await is_gbanned_user(CallbackQuery.from_user.id):
             return await CallbackQuery.answer(
-                "ʏᴏᴜ'ʀᴇ ɢʙᴀɴɴᴇᴅ", show_alert=True
+                "Yᴏᴜ'ʀᴇ ɢʙᴀɴɴᴇᴅ ʙʏ ᴛʜᴇ ʙᴏᴛ ᴀᴅᴍɪɴs", show_alert=True
             )
         return await mystic(_, CallbackQuery)
 
