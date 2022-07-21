93bd993ea9c894385cf9c073832a93b813d40262
SRUJANSBOT <97078395+SRUJANSBOT@users.noreply.github.com>
2022-07-21 09:54:26 +0530

    Update videocalls.py

--- Hero/Inline/videocalls.py
+++ Hero/Inline/videocalls.py
@@ -6,17 +6,17 @@ def choose_markup(videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🎵 ᴘʟᴀʏ ᴍᴜsɪᴄ",
+                text="🎶 Pʟᴀʏ ᴍᴜsɪᴄ",
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🎥 ᴘʟᴀʏ ᴠɪᴅᴇᴏ",
-                callback_data=f"Choose {videoid}|{duration}|{user_id}",
+                text=" 🥀 Sᴜᴘᴘᴏʀᴛ ",
+                url="https://t.me/Teddysupport",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🗑 ᴄʟᴏsᴇ sᴇᴀʀᴄʜ",
+                text="🗑 Cʟᴏsᴇ sᴇᴀʀᴄʜ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
@@ -28,11 +28,11 @@ def livestream_markup(quality, videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🎥  sᴛᴀʀᴛ ʟɪᴠᴇ",
+                text="🎥  Sᴛᴀʀᴛ ʟɪᴠᴇ",
                 callback_data=f"LiveStream {quality}|{videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🗑 ᴄʟᴏsᴇ",
+                text="🗑 Cʟᴏsᴇ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
@@ -58,11 +58,11 @@ def stream_quality_markup(videoid, duration, user_id):
         ],
         [
             InlineKeyboardButton(
-                text="⬅️ ɢᴏ ʙᴀᴄᴋ",
+                text="⬅️Gᴏ ʙᴀᴄᴋ",
                 callback_data=f"gback_list_chose_stream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🗑 ᴄʟᴏsᴇ sᴇᴀʀᴄʜ",
+                text="🗑 Cʟᴏsᴇ sᴇᴀʀᴄʜ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
